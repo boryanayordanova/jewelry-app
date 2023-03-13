1fade6ff3f05c60d95f1bf9eee8b283e9efc67a7
boryana.yordanova <boryana.yordanova@flatrocktech.com>
2023-03-13 17:51:16 +0200

    add form appearing

--- src/components/JewelryList.js
+++ src/components/JewelryList.js
@@ -1,16 +1,47 @@
 import { useState, useEffect } from "react";
 import * as FirebaseFetchData from './FirebaseFetchData';
 import JewelryItem from "./JewelryItem";
+import { JewelryCreate } from "./JewelryCreate";
 import styles from "./JewelryList.module.css";
 
 
-export default function JewelryList ({jewelry}) {
+export default function JewelryList ({
+    jewelry,
+    formValues,
+    formChangeHandler,
+    formErrors,
+    formValidate,
+    }) {
+
+    const [showAddJewelry, setShowAddJewelry] = useState(false);
+    
+
+
+    const onJewelryAddClick = () => {
+        setShowAddJewelry(true);
+    };
+
+    const onClose = () => {      
+        setShowAddJewelry(false);
+       
+    };
     
     return (
         <>
         <h2>List here</h2>
 
-        <button>Add New</button>
+        {showAddJewelry &&
+                <JewelryCreate
+                    onClose={onClose}
+                    // onUserCreateSubmit={onUserCreateSubmitHandler}
+                    formValues={formValues}
+                    formChangeHandler={formChangeHandler}
+                    formErrors={formErrors}
+                    formValidate={formValidate}
+                />
+            }
+
+        <button className={styles["btn-add-new"]} onClick={onJewelryAddClick}>Add New</button>
 
         <div className={styles["jewelry-items-container"]}>
             {jewelry.map(jew => (
